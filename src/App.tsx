c3703fdd24735c9551d054708bb94ca1cacbf69e
MRishabh23 <rmalik@justransform.com>
2023-09-16 17:26:21 +0530

    login bug fix

--- src/App.tsx
+++ src/App.tsx
@@ -46,20 +46,25 @@ const App: React.FC<props> = () => {
         <main className="flex-1 p-4 overflow-auto">
           <Routes>
             <Route path="/" element={<MainPage />} />
-            <Route path="/air">
-              <Route index element={<AirDashboard />} />
-              <Route path="latency" element={<AirLatency />} />
-            </Route>
-            <Route path="/ocean">
-              <Route index element={<OceanDashboard />} />
-              <Route path="latency" element={<OceanLatency />} />
-            </Route>
-            <Route path="/reference">
-              <Route index element={<ReferenceDashboard />} />
-              <Route path="list" element={<ReferenceList />} />
-              <Route path="history" element={<ReferenceHistory />} />
-            </Route>
-            <Route path="*" element={<Navigate to="/" />} />
+            {hasAuth === true ? (
+              <>
+                <Route path="/air">
+                  <Route index element={<AirDashboard />} />
+                  <Route path="latency" element={<AirLatency />} />
+                </Route>
+                <Route path="/ocean">
+                  <Route index element={<OceanDashboard />} />
+                  <Route path="latency" element={<OceanLatency />} />
+                </Route>
+                <Route path="/reference">
+                  <Route index element={<ReferenceDashboard />} />
+                  <Route path="list" element={<ReferenceList />} />
+                  <Route path="history" element={<ReferenceHistory />} />
+                </Route>
+              </>
+            ) : (
+              <Route path="*" element={<Navigate to="/" />} />
+            )}
           </Routes>
         </main>
         <Footer />
